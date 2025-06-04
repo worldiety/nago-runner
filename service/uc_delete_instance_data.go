37c1142b230cd545d4da4f21adc859a8efc41732
Torben Schinke <torben.schinke@worldiety.de>
2025-06-04 18:52:58 +0200

    fix: deleting data directory must resolve symlink due to systemd sandbox mount

--- service/uc_delete_instance_data.go
+++ service/uc_delete_instance_data.go
@@ -12,6 +12,7 @@ import (
 	"github.com/worldiety/nago-runner/service/event"
 	"log/slog"
 	"os"
+	"path"
 	"path/filepath"
 	"time"
 )
@@ -28,7 +29,18 @@ func NewDeleteInstanceData() DeleteInstanceData {
 
 		// TODO we don't have access to the actual systemd configuration here, we blindly delete by convention
 		path := filepath.Join("/var/lib/ngr", req.Unit)
+
 		slog.Warn("trying to delete service data dir by convention", "path", path)
+
+		if resolved, err := resolveLink(path); err == nil {
+			slog.Info("resolved data sym link", "path", path, "resolved", resolved)
+			if err := os.RemoveAll(resolved); err != nil {
+				slog.Error("failed to delete service data dir", "resolved", resolved)
+			} else {
+				slog.Info("service data dir deleted", "resolved", resolved)
+			}
+		}
+
 		if err := os.RemoveAll(path); err != nil {
 			slog.Error("failed to delete service data dir", "path", path)
 		} else {
@@ -41,4 +53,16 @@ func NewDeleteInstanceData() DeleteInstanceData {
 
 		return nil
 	}
+}
+
+func resolveLink(symLink string) (string, error) {
+	resolvedLink, err := os.Readlink(symLink)
+	if err != nil {
+		return "", err
+	}
+	if !filepath.IsAbs(resolvedLink) { // Output of os.Readlink is OS-dependent...
+		resolvedLink = path.Join(path.Dir(symLink), resolvedLink)
+	}
+
+	return resolvedLink, nil
 }
